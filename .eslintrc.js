a85779dd3b07a8348bdca4445dfd3e707547d739
Anotida <122183219+traejiik@users.noreply.github.com>
2025-06-14 07:21:13 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'func-names': 'off',
+    'no-process-exit': 'off',
+    'object-shorthand': 'off',
+    'class-methods-use-this': 'off',
+    'no-param-reassign': ['error', { props: false }],
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    'node/no-unsupported-features/es-syntax': 'off',
+    'max-classes-per-file': ['error', 2],
+  },
+  plugins: ['prettier'],
+  extends: ['airbnb', 'prettier', 'plugin:node/recommended'],
+};
